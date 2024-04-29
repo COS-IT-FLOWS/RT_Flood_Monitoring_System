543c6a8335810ef3f0bd7aa0fed2b68d129c6592
reticent-roklimber <dev.equinoct@gmail.com>
2024-04-29 17:50:45 +0530

    Remove __pycache__ dirs and .pyc files

--- apps/Sidebar/home.py
+++
@@ -1,8 +0,0 @@
-from dash import dcc, html
-from datetime import datetime, timedelta
-
-# sidebar to web app
-def home_sidebar():
-    bar = html.Div([
-    ], id="sidebar-home")
-    return bar
--- assets/dashExtensions_default.js
+++
@@ -1,7 +0,0 @@
-window.dashExtensions = Object.assign({}, window.dashExtensions, {
-    default: {
-        function0: function(feature, context) {
-            return context.props.hideout.Basin_ID == feature.properties.Basin_ID;
-        }
-    }
-});
